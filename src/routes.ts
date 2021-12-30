5a7fef9e03352cedb3a84343706641085534da42
MarcosFelipeDSilva <marcos.felipe01@hotmail.com>
2021-12-30 13:01:45 -0300

    create routes & controllers

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { Router } from "express";
+
+const routes = Router()
+
+routes.post("/categories", new CreateCategoryController().handle)
+
+export {routes}
